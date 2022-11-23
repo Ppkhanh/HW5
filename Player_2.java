f5cb27cad2d2ccfb9acb0012b610c8426acb70a6
Ppkhanh <96438319+Ppkhanh@users.noreply.github.com>
2022-11-23 10:38:11 +0100

    Update Player_2.java

--- Player_2.java
+++ Player_2.java
@@ -8,27 +8,27 @@ public class Player_2 extends Player{
         super.bet();
     }
     public void pickCard(){
-        int rand = 0;
+        //int rand = 0;
         String ans = "";
         int count = 0;
        while(!ans.equals("yes") || count<=5){
         if(count==5){
             System.out.println("You've run out of turns");
-            System.out.println("You must keep the number "+ rand);
+            System.out.println("You must keep the number "+ super.generatedNumber);
             break;
         }
         if(ans.equals("yes")){
             break;
         }
        super.pickCard();
-          System.out.println("Your number is "+ rand);
+          System.out.println("Your number is "+ super.generatedNumber);
           System.out.println("Are you satisfied with your number \n" +"***Only answer with 'yes' or 'no'");
              // Create a Scanner object
           Scanner scan = new Scanner(System.in); 
           ans = scan.nextLine();
           count++;
        } 
-       super.generatedNumber = rand;
+    //    super.generatedNumber = rand;
     }
 
 
